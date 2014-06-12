bb9c20a02227211a9fa157022d4b9e3aaa5eea32
vinhtran <vinhtran@teracy.com>
2014-06-12 23:10:12 +0700

    DEV-66 | Fix Travis CI build

--- main-cookbooks/teracy-dev/recipes/git-config.rb
+++ main-cookbooks/teracy-dev/recipes/git-config.rb
@@ -34,7 +34,7 @@
 
 # JUST change the attribute version below to change the git version. No more work here
 node.override['git']['version'] = '2.0.0'
-node.override['git']['url'] = 'https://www.kernel.org/pub/software/scm/git/git-#{node['git']['version']}.tar.gz'
+node.override['git']['url'] = "https://www.kernel.org/pub/software/scm/git/git-#{node['git']['version']}.tar.gz"
 
 include_recipe 'git::source'
 
